8fbe8cd7d3a751534d71b047b721ac5850d2ccdf
odwanodada <odwanodada22@gmail.com>
2021-10-21 16:15:39 +0200

    OdwaApp

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Page</h1>
+    <p>
+      Fugiat enim pariatur incididunt in esse labore amet eu adipisicing non.
+      Enim sit mollit non mollit sunt dolore nostrud. Anim consectetur Lorem
+      velit minim minim excepteur nostrud id.
+    </p>
+    <p>
+      Fugiat enim pariatur incididunt in esse labore amet eu adipisicing non.
+      Enim sit mollit non mollit sunt dolore nostrud. Anim consectetur Lorem
+      velit minim minim excepteur nostrud id.
+    </p>
+    <p>
+      Fugiat enim pariatur incididunt in esse labore amet eu adipisicing non.
+      Enim sit mollit non mollit sunt dolore nostrud. Anim consectetur Lorem
+      velit minim minim excepteur nostrud id.
+    </p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <v-list subheader two-line flat>
+
+      <v-list-item-group v-model="settings" multiple>
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>Wake Up</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
